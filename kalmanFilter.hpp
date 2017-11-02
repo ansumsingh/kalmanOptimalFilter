534bda22556160d9436d7e19bb2818be298a273b
Ansu Man SIngh <ansumsingh@gmail.com>
2017-11-03 10:47:10 +1100

    Add files via upload

--- kalmanFilter.hpp
+++ kalmanFilter.hpp
@@ -6,8 +6,8 @@
 #ifndef KALMANFILTER_HPP
 #define KALMANFILTER_HPP
 
-#include<eigen3/Eigen/Dense>
-#include<eigen3/Eigen/Core>
+#include<Eigen/Dense>
+#include<Eigen/Core>
 #include<iostream>
 
 
@@ -28,21 +28,23 @@ public:
         A(states,states),
         B(states,inputs),
         C(measurements,states),
+		D(measurements,inputs),
         Q(states,states),
         R(measurements,measurements),
-        P(states,states),
+		P(kalmanMatrix::Identity(states, states)),
         pre_state(states,1),
         post_state(states,1){};
     //
     //Generalized constructor
-    template<typename D>
-    kalmanFilter(const kalmanFilter<D> &other):
+    template<typename E>
+    kalmanFilter(const kalmanFilter<E> &other):
         states(other.getNoOfStates()),
         measurements(other.getNoOfMeasurements()),
         inputs(other.getNoOfInputs()),
         A(other.getMatrixA().template cast<T>()),
         B(other.getMatrixB().template cast<T>()),
         C(other.getMatrixC().template cast<T>()),
+		D(other.getMatrixD().template cast<T>()),
         Q(other.getMatrixQ().template cast<T>()),
         R(other.getMatrixR().template cast<T>()),
         P(other.getMatrixP().template cast<T>()),
@@ -58,6 +60,7 @@ public:
                   const kalmanMatrix &A,
                   const kalmanMatrix &B,
                   const kalmanMatrix &C,
+				  const kalmanMatrix &D,
                   const kalmanMatrix &Q,
                   const kalmanMatrix &R,
                   const kalmanMatrix &pre_state):
@@ -67,6 +70,7 @@ public:
         A(A),
         B(B),
         C(C),
+		D(D),
         Q(Q),
         R(R),
         P(kalmanMatrix::Identity(states,states)),
@@ -76,6 +80,7 @@ public:
                    (A.cols()==states)&&(A.rows()==states)&&
                    (B.rows()==states)&&(B.cols()==inputs)&&
                    (C.cols()==states)&&(C.rows()==measurements)&&
+				   (D.cols() ==inputs) && (D.rows() == measurements) &&
                    (Q.rows()==states)&&(Q.cols()==states)&&
                    (R.cols()==measurements)&&(R.rows()==measurements)&&
                    (pre_state.rows()==states)&&(pre_state.cols()==1));
@@ -89,50 +94,61 @@ public:
     const kalmanMatrix& getMatrixA()const{return A;}
     const kalmanMatrix& getMatrixB()const {return B;}
     const kalmanMatrix& getMatrixC()const {return C;}
+	const kalmanMatrix& getMatrixD()const { return D; }
     const kalmanMatrix& getMatrixQ()const{return Q;}
     const kalmanMatrix& getMatrixR()const{return R;}
     const kalmanMatrix& getMatrixP()const{return P;}
     const kalmanMatrix& getPreState()const{return pre_state;}
     const kalmanMatrix& getPostState()const{return post_state;}
+	const kalmanMatrix  getCurOutput()const{ return C*post_state + D*input; }
     //set the transitional Matrix;
     void setMatrixA(const kalmanMatrix &A_ip)
-                    {   assert((A.cols()==states)&&(A.rows()==states));
+                    {   assert((A_ip.cols()==states)&&(A_ip.rows()==states));
                         A=A_ip;}
     //set the Input matrix
     void setMatrixB(const kalmanMatrix &B_ip)
-                    {   assert((B.cols()==inputs)&&(A.rows()==states));
+                    {   assert((B_ip.cols()==inputs)&&(B_ip.rows()==states));
                         B=B_ip;}
     //Set the Output matrix
     void setMatrixC(const kalmanMatrix &C_ip)
-                    {   assert((C.cols()==measurements)&&(C.rows()==states));
+                    {   assert((C_ip.cols()==states)&&(C_ip.rows()==measurements));
                         C=C_ip;}
+
+	//Set input to output matrix
+	void setMatrixD(const kalmanMatrix &D_ip)
+	{
+		assert((D_ip.cols() == inputs) && (D_ip.rows() == measurements));
+		D = D_ip;
+	}
     //Set the process noise covariance
     void setMatrixQ(const kalmanMatrix &Q_ip)
-                    {   assert((Q.cols()==states)&&(Q.rows()==states));
+                    {   assert((Q_ip.cols()==states)&&(Q_ip.rows()==states));
                         Q=Q_ip;}
     //Set the measurement noise covariance
     void setMatrixR(const kalmanMatrix &R_ip)
-                    {   assert((R.cols()==measurements)&&(Q.rows()==measurements));
+                    {   assert((R_ip.cols()==measurements)&&(R_ip.rows()==measurements));
                         R=R_ip;}
     //Set the pre state
     void setMatrixPreState(const kalmanMatrix &pre_state_ip)
-                    {   assert((R.cols()==measurements)&&(Q.rows()==measurements));
+                    {   assert(pre_state_ip.rows()==states);
                         pre_state=pre_state_ip;}
 
     //Predict the value
-    const kalmanMatrix& predict(const kalmanMatrix& input)
-                     { assert(input.rows()==inputs);
+    const kalmanMatrix& predict(const kalmanMatrix& ip)
+                     { assert(ip.rows()==inputs);
+					   input = ip;
                        P = A*P*Transpose<kalmanMatrix>(A)+Q;
                        post_state = A*pre_state +B*input;
                        pre_state = post_state;
                        return post_state;
     }
     //Correction steps
-    const kalmanMatrix& correct(const kalmanMatrix& measurement,const kalmanMatrix& input)
+    const kalmanMatrix& correct(const kalmanMatrix& measurement,const kalmanMatrix& ip)
                      {
-                        assert(input.rows()==inputs&&measurement.rows()==measurements);
+                        assert(ip.rows()==inputs&&measurement.rows()==measurements);
 
                         P = A*P*A.transpose()+Q;
+						input = ip;
                         //Inovation and optimal kalman gain calculation
                         const kalmanMatrix K = P*C.transpose()*(C*P*C.transpose()+R).inverse();
                         //Update or the correction step
@@ -142,6 +158,7 @@ public:
                         //update covariance step
                         P =(kalmanMatrix::Identity(states,states)-K*C)*P;
                         pre_state=post_state;
+						
                         return post_state;
                         }
 
@@ -160,11 +177,13 @@ private:
         kalmanMatrix A;
         kalmanMatrix B;
         kalmanMatrix C;
+		kalmanMatrix D;
         kalmanMatrix Q;
         kalmanMatrix R;
         kalmanMatrix P;
         kalmanMatrix pre_state;
         kalmanMatrix post_state;
+		kalmanMatrix input;
 };
 
 //For displaying all the parameters of the filter
